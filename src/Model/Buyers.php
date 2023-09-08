557afd045bc91efff8baba7630ea4096254271c1
Nolazco <carlos_sceptile@hotmail.com>
2023-09-08 12:32:31 -0600

    feat: Buyers model created

---
+++ src/Model/Buyers.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Model;
+
+class Buyers{
+
+    /**
+     * @param int $uid
+     * @return array
+     */
+    public function create_buyers($uid): array{
+		return ["INSERT INTO buyers('user_id') VALUES(?)"];
+	}
+
+    /**
+     * @param int $id
+     * @param bool $state
+     * @return array
+     */
+    public function
+     change_state($id, $state): array{
+		return ["UPDATE sellers SET state = ? WHERE id = ?"];
+	}
+}
