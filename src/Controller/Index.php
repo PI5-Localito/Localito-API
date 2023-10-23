88478bfc92de3755fd0db9a381534ee775190dc9
Nolazco <carlos_sceptile@hotmail.com>
2023-10-23 12:29:34 -0600

    feat: stands crud created

--- src/Controller/Index.php
+++ src/Controller/Index.php
@@ -32,4 +32,19 @@ class Index extends AbstractController {
 	public function edit_user(): Response {
 		return $this->render('edit_user.html.twig');
 	}
+
+	#[Route('/stands', methods: 'GET')]
+	public function stands(): Response {
+		return $this->render('stands.html.twig');
+	}
+
+	#[Route('/stand', methods: 'GET')]
+	public function view_stand(): Response {
+		return $this->render('view_stand.html.twig');
+	}
+
+	#[Route('/editarstand', methods: 'GET')]
+	public function edit_stand(): Response {
+		return $this->render('edit_stand.html.twig');
+	}
 }
