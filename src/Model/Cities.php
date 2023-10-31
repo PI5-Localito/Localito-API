bdaae64fb91c55b1afb9eda8c9b82b00c4e3cc90
Nolazco <carlos_sceptile@hotmail.com>
2023-10-31 07:42:56 -0600

    feat: city model updated

--- src/Model/Cities.php
+++ src/Model/Cities.php
@@ -2,23 +2,115 @@
 
 namespace App\Model;
 
-class Cities{
+use App\Entity\City;
+use Lib\Storage\Entity;
+use Lib\Storage\Model;
+use Lib\Storage\Traits\DefaultModel;
+use PDO;
+use PDOStatement;
+
+class Cities implements Model{
+
+    use DefaultModel;
 
     /**
-     * @param string $cCode
-     * @param string $name
-     * @param float $long
-     * @param float $lat
-     * @return array
+     * @param PDOStatement $stmt
+     * @param City $entity
+     *
+     * @return void
      */
-    public function add_city($cCode, $name, $long, $lat): array{
-		return ["INSERT INTO cities(country_code, city_name, longitude, latitude) VALUES(?, ? ,? ,?)"];
-	}
+    public function bindValues(PDOStatement $stmt, Entity $entity): void
+    {
+        $stmt->bindValue('bundle__countryCode', $entity->getCountry(), PDO::PARAM_STR);
+        $stmt->bindValue('bundle__cityName', $entity->getName(), PDO::PARAM_STR);
+        $stmt->bindValue('bundle__long', $entity->getLong(), PDO::PARAM_STR);
+        $stmt->bindValue('bundle__lat', $entity->getLat(), PDO::PARAM_STR);
+    }
+
+    public function all(int $limit = 0, int $offset = 0, int $page = 0): array
+    {
+        $stmt = $this->getAll($limit, $offset, $page);
+        $entities = [];
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $entity = new City();
+            $entities[] = $entity->hydrate($row);
+        }
+
+        return $entities;
+    }
+
+    public function get(int $id): ?City
+    {
+        $row = $this->getByID($id);
+        if($row === false){
+            return null;
+        }
+        $entity = new City();
+        $entity->hydrate($row);
+    }
+
     /**
-     * @param int $id
-     * @return array
+     * @param City $entity
+     *
+     * @return bool
      */
-    public function select_city($id): array{
-		return ["SELECT * FROM cities WHERE id = ?"];
-	}
+    public function save(Entity $entity): bool
+    {
+        $stmt = $this->db->prepare(
+            "INSERT INTO `{$this->getTable()}` (" .
+                'country_code,' .
+                'city_name,' .
+                'longitude,' .
+                'latitude' .
+            ') VALUES (' .
+                ':bundle__countryCode, ' .
+                ':bundle__cityName,' .
+                ':bundle__long,' .
+                ':bundle__lat' .
+                ')'
+        );
+        $this->bindValues($stmt, $entity);
+        $stmt->execute();
+        $id = $this->db->lastInsertId();
+        $entity->setId($id);
+        return true;
+    }
+
+    /**
+     * @param City $entity
+     *
+     * @return bool
+     */
+    public function update(Entity $entity): bool
+    {
+        $stmt = $this->db->prepare(
+            "UPDATE `{$this->getTable()}` SET " . 
+                'country_code = :bundle__countryCode, ' .
+                'city_name = :bundle__cityName, ' .
+                'longitude = :bundle__long, ' .
+                'latitude = :bundle__lat ' .
+            'WHERE id = ?'
+        );
+        $this->bindValues($stmt, $entity);
+        $this->bindValues(0, $entity->getId());
+        $stmt->execute();
+        return true;
+    }
+
+    /**
+     * @param City $entity
+     *
+     * @return bool
+     */
+    public function delete(Entity $entity): bool
+    {
+        $stmt = $this->db->prepare("DELETE FROM `{$this->getTable()}` WHERE id=?");
+        $stmt->bindValue(0, $entity->getId());
+        return $stmt->execute();
+    }
+
+    public static function getTable(): string
+    {
+        return 'cities';
+    }
 }
