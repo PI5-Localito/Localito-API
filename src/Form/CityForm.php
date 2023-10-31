2bacd326c22d3d63869e5512c3055f13487383c3
Nolazco <carlos_sceptile@hotmail.com>
2023-10-31 11:32:44 -0600

    feat: city form created

---
+++ src/Form/CityForm.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class CityForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('submit', SubmitType::class)
+            ->add('countryCode', TextType::class)
+            ->add('cityName', TextType::class)
+            ->add('longitude', NumberType::class)
+            ->add('latitude', NumberType::class)
+    }
+}
