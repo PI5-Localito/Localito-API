651daed06b84d05754c850b536278766d79be607
Nolazco <carlos_sceptile@hotmail.com>
2023-10-31 09:32:57 -0600

    feat: orders model updated

--- src/Model/Orders.php
+++ src/Model/Orders.php
@@ -2,39 +2,118 @@
 
 namespace App\Model;
 
+use App\Entity\Order;
+use Lib\Storage\Entity;
+use Lib\Storage\Model;
+use Lib\Storage\Traits\DefaultModel;
+use PDO;
+use PDOStatement;
+
 class Orders{
-    /**
-     * @param int $bid
-     * @param int $sid
-     * @param int $stid
-     * @return array
-     */
-    public function create_order($bid, $sid, $stid): array{
-		return ["INSERT INTO orders('buyer_id', 'seller_id', 'stand_id') VALUES(?, ?, ?)"];
-	}
+    use DefaultModel;
 
     /**
-     * @param int $id
-     * @return array
+     * @param PDOStatement $stmt
+     * @param Order $entity
+     *
+     * @return void
      */
-    public function view_order($id): array{
-		return ["SELECT * FROM orders WHERE 'id' = ?"];
-	}
+    public function bindValues(PDOStatement $stmt, Entity $entity): void
+    {
+        $stmt->bindValue('bundle__buyerId', $entity->getBuyer(), PDO::PARAM_INT);
+        $stmt->bindValue('bundle__sellerId', $entity->getSeller(), PDO::PARAM_INT);
+        $stmt->bindValue('bundle__standId', $entity->getSeller(), PDO::PARAM_INT);
+        $stmt->bindValue('bundle__date', $entity->getDate(), PDO::PARAM_STR);
+        $stmt->bindValue('bundle__state', $entity->getState(), PDO::PARAM_STR);
+    }
+
+    public function all(int $limit = 0, int $offset = 0, int $page = 0): array
+    {
+        $stmt = $this->getAll($limit, $offset, $page);
+        $entities = [];
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $entity = new Order();
+            $entities[] = $entity->hydrate($row);
+        }
+
+        return $entities;
+    }
+
+    public function get(int $id): ?Order
+    {
+        $row = $this->getByID($id);
+        if($row === false){
+            return null;
+        }
+        $entity = new Order();
+        $entity->hydrate($row);
+    }
 
     /**
-     * @param int $id
-     * @param string $state
-     * @return array<int,string>
+     * @param Order $entity
+     *
+     * @return bool
      */
-    public function change_state($id, $state): array{
-		return ["UPDATE orders SET 'state' = ? WHERE id = ?"];
-	}
+    public function save(Entity $entity): bool
+    {
+        $stmt = $this->db->prepare(
+            "INSERT INTO `{$this->getTable()}` (" .
+                'buyer_id, ' .
+                'seller_id, ' .
+                'stand_id, ' .
+                'date, ' .
+                'state' .
+            ') VALUES (' .
+                ':bundle__buyerId, ' .
+                ':bundle__sellerId,' .
+                ':bundle__standId,' .
+                ':bundle__date,' .
+                ':bundle__state' .
+            ')'
+        );
+        $this->bindValues($stmt, $entity);
+        $stmt->execute();
+        $id = $this->db->lastInsertId();
+        $entity->setId($id);
+        return true;
+    }
 
     /**
-     * @param int $id
-     * @return array
+     * @param Order $entity
+     *
+     * @return bool
      */
-    public function delete_order($id): array{
-		return ["DELETE FROM orders WHERE 'id' = ?"];
-	}
+    public function update(Entity $entity): bool
+    {
+        $stmt = $this->db->prepare(
+            "UPDATE `{$this->getTable()}` SET " .
+            'buyer_id = :bundle__buyerId, ' .
+            'seller_id = :bundle__sellerId, ' .
+            'stand_id = :bundle__standId, ' .
+            'date = :bundle__date, ' .
+            'state = :bundle__state ' .
+            'WHERE id = ?'
+        );
+        $this->bindValues($stmt, $entity);
+        $this->bindValues(0, $entity->getId());
+        $stmt->execute();
+        return true;
+    }
+
+    /**
+     * @param Order $entity
+     *
+     * @return bool
+     */
+    public function delete(Entity $entity): bool
+    {
+        $stmt = $this->db->prepare("DELETE FROM `{$this->getTable()}` WHERE id=?");
+        $stmt->bindValue(0, $entity->getId());
+        return $stmt->execute();
+    }
+
+    public static function getTable(): string
+    {
+        return 'orders';
+    }
 }
